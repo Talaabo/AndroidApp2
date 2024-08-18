4e60965d2ada1a80af0182bf0563bdf517ee6213
Mustafe Ahmed <160895798+Talaabo@users.noreply.github.com>
2024-08-18 12:23:14 -0400

    added classes

---
+++ listmaker2/app/src/main/java/com/trios2024ammy/listmaker2/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.trios2024ammy.listmaker2.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder (val binding: ListSelectionViewHolderBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+}
--- listmaker2/app/src/main/java/com/trios2024ammy/listmaker2/ui/main/MainFragment.kt
+++ listmaker2/app/src/main/java/com/trios2024ammy/listmaker2/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.trios2024ammy.listmaker2.R
 import com.trios2024ammy.listmaker2.databinding.FragmentMainBinding
@@ -29,6 +30,9 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+
         return binding.root
     }
 
