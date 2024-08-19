9c00fbed01fbf3dc5c4913e1512bb01d5eab4ce3
Mustafe Ahmed <160895798+Talaabo@users.noreply.github.com>
2024-08-19 01:34:58 -0400

    Display initial list

--- listmaker2/app/src/main/java/com/trios2024ammy/listmaker2/ui/main/ListSelectionViewHolder.kt
+++ listmaker2/app/src/main/java/com/trios2024ammy/listmaker2/ui/main/ListSelectionViewHolder.kt
@@ -1,6 +1,7 @@
 package com.trios2024ammy.listmaker2.ui.main
 
 import androidx.recyclerview.widget.RecyclerView
+import com.trios2024ammy.listmaker2.databinding.ListSelectionViewHolderBinding
 
 class ListSelectionViewHolder (val binding: ListSelectionViewHolderBinding) :
     RecyclerView.ViewHolder(binding.root) {
